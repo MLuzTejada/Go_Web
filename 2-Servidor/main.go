54d08f860df86b44c27b5db6466a2b812a275ef3
Maria Luz <marialuztejada055@gmail.com>
2020-09-28 15:09:57 -0300

    arreglado

--- 2-Servidor/main.go
+++ 2-Servidor/main.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 )
 
-func Hola(w http.ResponseWriter, r *http.Request) {
+func hola(w http.ResponseWriter, r *http.Request) { //el identificador de la función se pone en mayuscula para usarlo desde otros paquetes
 	fmt.Fprintf(w, "Hola Mundo")
 }
 
 func main() {
 
-	http.HandleFunc("/", Hola)
-	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+	http.HandleFunc("/", hola)
+	log.Fatal(http.ListenAndServe("localhost:3000", nil)) //como esta nil, se utiliza DefaultServerMux
 }
 
 //serverMux: listado de rutas con acciones asociadas
